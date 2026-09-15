f6a4431d111d394ecea69815d3d2d83f3ecc1d91
Nathan Berhanu <natib1928@gmail.com>
2026-09-15 17:31:09 +0300

    connecting to db

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
-const cors = require('cors')
+const Person = require('./models/person')
 const morgan = require('morgan')
+
 const app = express()
 
-app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
@@ -14,31 +15,19 @@ morgan.token('content', (request, response) =>{
   return JSON.stringify(body)
 })
 
+if(process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
 
-let Persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
-const Quantity = Persons.length
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+
+
+const Quantity = Person.length
 
 const generateId = () => {
   const id = Math.random() * 100
@@ -52,22 +41,25 @@ app.get('/', (request, response) => {
 
 
 app.get('/api/persons', (request, response) => {
-  response.json(Persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
-   response.send(`Phonebook has info for ${Quantity} people<br>${new Date()}`)
+   response.send(`Phonebook has info for ${Quantity} people ${new Date()}`)
 })
 
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = Persons.find(p => p.id === id)
-  if(person){
-    response.json(person)
-  }else{
-    response.status(404).end()
-  }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.json(person)
+      }
+  })
 })
 
 
@@ -76,41 +68,35 @@ app.post('/api/persons', (request, response) => {
 
   if(
     !body.name ||
-    !body.number ||
-    Persons.some(p => p.name === body.name) ||
-    Persons.some(p => p.number === body.number)
-  ){
+    !body.number
+  ){  
     return response.status(404).json({ error: 'empty parameter or already exisitng' })
 
-  }else{
-    const person = {
-      id: generateId(),
+  }
+  const person = new Person({
       name : body.name,
       number: body.number
-    }
-  
-    persons = Persons.concat(person)
-    
-    response.json(persons)
+  })
 
-  }
+  person.save().then(savedPersons => {
+     response.json(savedPersons)
+  })
+
 })
 
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = Persons.find(p => p.id === id)
-  const newPersons = Persons.filter(p => p.id !== id)
+app.delete('/api/persons/:id', async (request, response) => {
+  const person = await Person.findByIdAndDelete(request.params.id)
 
-
-  if(person){
-    response.status(204).end()
-  }else{
-    response.status(404).end()
+  if (!person) {
+    return response.status(404).end()
   }
+
+  response.status(204).end()
 })
 
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
