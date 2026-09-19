90b0ca7eb948905efc42dffa0bbaf9246254b300
Nathan Berhanu <natib1928@gmail.com>
2026-09-19 20:07:27 +0300

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -27,16 +27,6 @@ morgan.token('content', (request, response) =>{
   return JSON.stringify(body)
 })
 
-if(process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
-
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
-
 
 
 const Quantity = Person.length
