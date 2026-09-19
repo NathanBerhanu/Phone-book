69ae3befcfad484c5a023079061406c3e657430f
Nathan Berhanu <natib1928@gmail.com>
2026-09-19 18:57:11 +0300

    uibuild

--- backend/models/person.js
+++ backend/models/person.js
@@ -21,6 +21,13 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v){
+        return /\d{2,3}-\d{7,}/.test(v)
+      }
+    },
+    required: true
     
   }
 })
